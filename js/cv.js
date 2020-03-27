385de03f068f6ffea4659ceb2122a0d1b40be474
Ichen.tsu <r99521320@gmail.com>
2020-03-27 14:53:26 +0800

    [Feature] CV add timelineJS for recently news.

---
+++ js/cv.js
@@ -0,0 +1,38 @@
+(function () {
+
+    var originSize;
+    var preDOM;
+
+    $("body").on("UPDATE", ".vco-slider", function (event, page) {
+        // console.log(page);
+        if (preDOM) preDOM.css({fontSize: originSize})
+
+        var operate = eval("$('#time" + page + "')");
+        originSize = operate.css("fontSize")
+        preDOM = operate
+
+        operate.css({fontSize: "16pt"});
+
+    });
+
+    $("body").on("UPDATE", ".vco-navigation", function (event, page) {
+        // console.log(page);
+        if (preDOM) preDOM.css({fontSize: originSize})
+
+        var operate = eval("$('#time" + page + "')");
+        originSize = operate.css("fontSize")
+        preDOM = operate
+
+        operate.css({fontSize: "16pt"});
+    });
+
+
+    $("body").on("LOADED", ".vco-slider", function (event) {
+        console.log("L1")
+    });
+
+    $("body").on("LOADED", ".vco-navigation", function (event) {
+        console.log("L2")
+    });
+
+})();
